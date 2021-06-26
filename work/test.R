d9d502f7a537a9fb688ab24fee27e33586e2f620
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2021-06-26 13:27:24 +0200

    Test link

--- work/test.R
+++ work/test.R
@@ -80,3 +80,77 @@ select_n_to_m(pairs, "selected_ntom", "weight", preselect = "selected",
 table(pairs$selected, pairs$selected_ntom)
 
 
+
+
+link <- function(pairs, selection = NULL, all_x = TRUE, all_y = TRUE, 
+    x = attr(pairs, "x"), y = attr(pairs, "y"), suffixes = c(".x", ".y"),
+    keep_from_pairs = NULL) {
+  x$.x <- seq_len(nrow(x))
+  y$.y <- seq_len(nrow(y))
+  if (!missing(selection) && !is.null(selection)) {
+    if (is.character(selection)) {
+      stopifnot(selection %in% names(pairs))
+      selection <- pairs[[selection]]
+    }
+  } else selection <- TRUE
+  vars <- unique(c(".x", ".y", keep_from_pairs))
+  res <- merge(pairs[selection == TRUE, ..vars], x, all.x = TRUE, 
+    all.y = all_x, by = ".x", suffixes = c("_pairs", ""))
+  res <- merge(res, y, all.x = TRUE, all.y = all_y, by = ".y",
+    suffixes = suffixes)
+  res
+}
+
+
+res <- link(pairs, selection = "selected_ntom", x = x, y = y, all_x = TRUE, all_y = FALSE)
+
+table(is.na(res$id.x), is.na(res$id.y))
+
+nrow(x)
+
+x
+x[, .x := .I]
+y[, .y := .I]
+
+?.N
+
+
+
+
+
+link_impl <- function(pairs, selection = NULL, x = NULL, y = NULL, 
+    all_x = TRUE, all_y = TRUE) {
+  # Process x and y
+  if (missing(x) || is.null(x)) x <- attr(pairs, "x")
+  if (is.null(x)) stop("Missing x")
+  if (missing(y) || is.null(y)) y <- attr(pairs, "y")
+  if (is.null(y)) stop("Missing y")
+  x$.x <- seq_len(nrow(x))
+  y$.y <- seq_len(nrow(y))
+  # Process selection
+  if (missing(selection) || is.null(selection)) 
+    selection <- attr(pairs, "selection")
+  # (repeat previous if.. in case attribute was not set)
+  if (missing(selection) || is.null(selection)) {
+    selection <- TRUE
+  } else if (is.character(selection)) {
+    if (length(selection) != 1) 
+      stop("When selection is a character vector; it needs to be length 1.")
+    selection <- pairs[[selection]]
+  } 
+  # Link
+  res <- data.frame('.x' = as_rvec(pairs$x[selection]),
+    '.y' = as_rvec(pairs$y[selection]))
+  res <- if (all_x) dplyr::full_join(res, x, by = ".x") else
+    dplyr::left_join(res, x, by = ".x")
+  res <- if (all_y) dplyr::full_join(res, y, by = ".y") else
+    dplyr::left_join(res, y, by = ".y")
+  res$.x <- NULL
+  res$.y <- NULL
+  res
+  # The following gnerates note in R CMD check
+  # dplyr::select(res, -`.x`, -`.y`)
+}
+
+
+
